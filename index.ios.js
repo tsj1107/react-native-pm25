a9d396657f1f960c366032a22891d24c1cd3fe63
夏奈 <shuaiji.tsj@alibaba-inc.com>
2015-12-02 20:42:45 +0800

    轮播

--- index.ios.js
+++ index.ios.js
@@ -2,63 +2,94 @@
 
 var React = require('react-native');
 var { AppRegistry,
-      ScrollView,
-      Text,
-      StyleSheet,
-      Image,
-      View } = React;
+    ScrollView,
+    Text,
+    StyleSheet,
+    Image,
+    Dimensions,
+    View } = React;
 
 var THUMBS = ['http://facebook.github.io/react/img/logo_og.png',
+  'https://t.alipayobjects.com/images/T11rdgXbFkXXXXXXXX.png',
+  'http://facebook.github.io/react/img/logo_og.png',
+  'https://t.alipayobjects.com/images/T11rdgXbFkXXXXXXXX.png',
+  'http://facebook.github.io/react/img/logo_og.png',
+  'https://t.alipayobjects.com/images/T11rdgXbFkXXXXXXXX.png',
+  'http://facebook.github.io/react/img/logo_og.png',
+  'https://t.alipayobjects.com/images/T11rdgXbFkXXXXXXXX.png',
   'http://facebook.github.io/react/img/logo_og.png'];
-var createThumbRow = (uri, i) => <Thumb key={i} uri={uri} />;
 
-var ScrollViewSample = React.createClass({
-  render: function() {
+var createThumbRow = (uri, i) => <Thumb key={i} uri={uri}/>;
+var createDotRow = (uri, i) => function () {
+  if (i === 0) {
+    return <Dot selected={true} />
+  } else {
+    return <Dot />
+  }
+};
+var PM25 = React.createClass({
+  render: function () {
     return (
-      <ScrollView
-          automaticallyAdjustContentInsets={false}
-          horizontal={true}>
-        <Text>kkkkkk</Text>
-        <View>
-          <Image style={styles.img}
-                 source={{uri: 'http://img0.imgtn.bdimg.com/it/u=3710103736,733712610&fm=21&gp=0.jpg'}}
-              />
+        <View style={{backgroundColor: '#000000'}}>
+          <ScrollView
+              showsHorizontalScrollIndicator={false}
+              showsVerticalScrollIndicator={false}
+              pagingEnabled={true}
+              automaticallyAdjustContentInsets={false}
+              horizontal={true}>
+            {THUMBS.map(createThumbRow)}
+          </ScrollView>
+          <View>
+            {THUMBS.map(createDotRow)}
+          </View>
         </View>
-        <View>
-          <Image
-              style={styles.img}
-              source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}}
-              />
-        </View>
-        {THUMBS.map(createThumbRow)}
-      </ScrollView>
     );
   }
 });
 
 var Thumb = React.createClass({
-  shouldComponentUpdate: function(nextProps, nextState) {
+  shouldComponentUpdate: function (nextProps, nextState) {
     return false;
   },
   render: function () {
     return (
-        <View>
-          <Image source={{uri: this.props.uri}}/>
+        <View style={styles.panel}>
+          <Image style={styles.img} source={{uri: this.props.uri}}/>
         </View>
     );
   }
 });
 
+var Dot = React.createClass({
+  propTypes: {
+    selected: false
+  },
+  render: function () {
+    let selected = this.props.selected;
+    return (
+        <View style={[styles.dot, { backgroundColor: selected? '#f3f3f3' : '#ffffff' }]}></View>
+    )
+  }
+});
+
 var styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: '#6A85B1',
-    height: 300,
+  dot: {
+    width: 10,
+    height: 10,
+    borderRadius: 5
+  },
+  panel: {
+    backgroundColor: '#cccccc',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   img: {
     width: 64,
-    height: 64,
+    height: 64
   }
 });
 
 
-AppRegistry.registerComponent('RNProject', () => ScrollViewSample);
+AppRegistry.registerComponent('RNProject', () => PM25);
